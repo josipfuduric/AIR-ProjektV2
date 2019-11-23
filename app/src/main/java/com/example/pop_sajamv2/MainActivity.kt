2c746ebdfbcbc3e4b18b48f48d47cf38459280f2
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2019-11-23 12:13:10 +0100

    Implementirano spremanje sesije nakon prijave

--- app/src/main/java/com/example/pop_sajamv2/MainActivity.kt
+++ app/src/main/java/com/example/pop_sajamv2/MainActivity.kt
@@ -35,8 +35,21 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
                 if(!response!!.body()!!.STATUS)
                     Toast.makeText(this@MainActivity,response!!.body()!!.STATUSMESSAGE,Toast.LENGTH_SHORT).show()
-                else
-                    Toast.makeText(this@MainActivity,"Uspjesna prijava",Toast.LENGTH_SHORT).show()
+                else {
+                    Toast.makeText(this@MainActivity, "Uspjesna prijava", Toast.LENGTH_SHORT).show()
+                    Session.user.Ime=response!!.body()!!.DATA!!.Ime
+                    Session.user.Prezime= response!!.body()!!.DATA!!.Prezime
+                    Session.user.Email = response!!.body()!!.DATA!!.Email
+                    Session.user.KorisnickoIme = response!!.body()!!.DATA!!.KorisnickoIme
+                    Session.user.Id_Uloge = response!!.body()!!.DATA!!.Id_Uloge
+                    Session.user.Naziv_Uloge = response!!.body()!!.DATA!!.Naziv_Uloge
+                    Session.user.StanjeRacuna = response!!.body()!!.DATA!!.StanjeRacuna
+                    Session.user.DozvolaPregledTransakcija = response!!.body()!!.DATA!!.DozvolaPregledTransakcija
+                    Session.user.DozvolaUpravljanjeStanjemRacuna = response!!.body()!!.DATA!!.DozvolaUpravljanjeStanjemRacuna
+                    Session.user.DozvolaUpravljanjeUlogama = response!!.body()!!.DATA!!.DozvolaUpravljanjeUlogama
+                    Session.user.DozvolaUvidUStatistiku = response!!.body()!!.DATA!!.DozvolaUvidUStatistiku
+                    Session.user.LoginTime = response!!.body()!!.DATA!!.LoginTime
+                }
             }
         })
 
